f448097e03bc0327c615a55f72c61ba0fb3d4623
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-04-16 23:38:35 -0400

    Complete lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,58 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET','POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.order_by(Message.created_at.asc()).all():
+            messages.append(message.to_dict())
+        return make_response(messages,200)
+    
+    elif request.method == 'POST':
+        json = request.get_json() # a js object | use [] to access value of body key 
+        new_message = Message(
+            body = json['body'],
+            username = json['username'])
+        
+        db.session.add(new_message)
+        db.session.commit()
 
-@app.route('/messages/<int:id>')
+        message_dict=new_message.to_dict()
+
+        return make_response(message_dict,201)
+    
+
+@app.route('/messages/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if request.method == 'GET':
+        message_dict = message.to_dict()
+        return make_response(message_dict,200)
+
+    elif request.method == 'PATCH':
+        json = request.get_json()
+        for attr in json:
+            setattr(message, attr, json[attr])
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict=message.to_dict()
+
+        return make_response(message_dict,200)
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_dict = {
+            "message_delete": True,
+            "body": "Message has been deleted!"
+        }
+
+        return make_response(response_dict,200)
 
 if __name__ == '__main__':
     app.run(port=5555)
